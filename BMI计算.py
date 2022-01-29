d775ab0fa0628d5e85dc80578715d549f175bb75
Magneto <48152281+ouyangyanhuo@users.noreply.github.com>
2022-01-29 04:44:25 -0600

    Update BMI计算.py

--- BMI计算.py
+++ BMI计算.py
@@ -24,14 +24,14 @@ the_number = float(number)
 print(f"\nHi {the_name}!Your BMI number is {the_number} \n")
 #分析
 if the_number < 18.5:
-  print(f"Oh!{the_name}!You're underweight")
+  print(f"Oh!{the_name}!You're underweight and BMI is below standerd.")
 elif 18.5 <= the_number <= 23.9:
-  print(f"{the_name},your BMI is normal")
+  print(f"{the_name},your BMI is normal.")
 elif 24 <= the_number <= 27.9:
-  print(f"{the_name},you're overweight")
+  print(f"{the_name},you're overweight and BMI is higher than the standard.")
 elif 28 <= the_number <= 32:
-  print(f"{the_name},you're too fat")
+  print(f"{the_name},you're too fat and BMI well above the mark")
 else:
-  print(f"{the_name},you're severely exeed the limit")
+  print(f"{the_name},you're severely exeed the limit please lose weight")
 print("\nThe norm come from PRC")
 x = input('Press Enter to exit')
